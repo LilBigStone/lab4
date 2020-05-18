a409c4e823d503e49e69b11b649a22a952418933
LilBigStone <56958986+LilBigStone@users.noreply.github.com>
2020-05-18 12:55:21 +0300

    Add files via upload

---
+++ lab3/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path, include
+from django.contrib.staticfiles.urls import static
+from . import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('mainApp.urls')),
+    path('news/', include('news.urls'))
+
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
